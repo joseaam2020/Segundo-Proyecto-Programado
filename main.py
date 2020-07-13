ac449e9e0a6c2eb6af664e34a9412997bd2f0d79
joseaam2020 <joseaam2019@hotmail.com>
2020-07-13 12:19:56 -0600

    Cambios en el mapa, se agregaron todos los escenarios

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from pathlib import Path
 
 #Iniciando Pygame y Clock
 clock = pygame.time.Clock()
@@ -10,52 +11,56 @@ screen = pygame.display.set_mode(size=window_size)
 pygame.display.set_caption("Proyecto Programado")
 
 #Creando Superficie 
-display = pygame.Surface((112,180))
+display = pygame.Surface((112,176))
 
 
 #Cargando Mapa
 def mapa(archivo):
     copia = open(archivo+".txt")
-    mapa = copia.read()
+    mapa_lista = copia.read()
     copia.close
-    return(mapa)
+    mapa_lista = mapa_lista.split('\n')
+    for y in range(len(mapa_lista)):
+        mapa_lista[y] = mapa_lista[y].split()
+    return(mapa_lista)
+
+#Cargando Mapa
+def cargar_img_mapa(carpeta):
+    path = Path(".")/carpeta
+    lista_archivos = list(path.iterdir())
+    lista_tiles = []
+    for file in lista_archivos:
+        lista= str(file).split()[0].split('\\')
+        nombre_img= lista[1].strip(".png")
+        print(nombre_img)
+        path = (lista[0] + '/' + lista[1])
+        print(path)
+        img = pygame.image.load(path).convert()
+        print(img)
+        img.set_colorkey((255,255,255))
+        lista_tiles.append([nombre_img,img])
+    return lista_tiles
+
+#print(mapa("mapa-prueba"))
+tiles = cargar_img_mapa("Tiles2")
 
 #Cargando Imagenes del menu
-scroll = pygame.image.load("Tiles1/frames1/scroll.png").convert()
+scroll = pygame.image.load("Tiles2/scroll.png").convert()
 scroll.set_colorkey((255,255,255))
 
 #Cargando Imagenes del escenario1
-casilla1 = pygame.image.load("Tiles1/frames1/floor_1.png").convert()
-casilla2 = pygame.image.load("Tiles1/frames1/floor_1.png").convert()
-pared_izquierda = pygame.image.load("Tiles1/frames1/wall_side_mid_left.png").convert()
-pared_derecha = pygame.image.load("Tiles1/frames1/wall_side_mid_right.png").convert()
-pared_centro = pygame.image.load("Tiles1/frames1/wall_mid.png").convert()
-esquina_izquierda = pygame.image.load("Tiles1/frames1/wall_inner_corner_mid_left.png").convert()
-esquina_derecha = pygame.image.load("Tiles1/frames1/wall_inner_corner_mid_rigth.png").convert()
-pared_arriba = pygame.image.load("Tiles1/frames1/wall_top_mid.png").convert()
-esquina_izquierda_arriba = pygame.image.load("Tiles1/frames1/wall_inner_corner_t_top_left.png").convert()
-esquina_derecha_arriba = pygame.image.load("Tiles1/frames1/wall_inner_corner_t_top_rigth.png").convert()
-
-pared_izquierda.set_colorkey((255,255,255))
-pared_derecha.set_colorkey((255,255,255))
-esquina_izquierda_arriba.set_colorkey((255,255,255))
-esquina_derecha_arriba.set_colorkey((255,255,255))
-pared_arriba.set_colorkey((255,255,255))
+casilla1= pygame.image.load("Tiles2/casilla1.png").convert()
+casilla2= pygame.image.load("Tiles2/casilla2.png").convert()
 
 #Cargando Imagenes del escenario2
-casilla3 = pygame.image.load("Tiles1/frames2/tile155.png").convert()
-casilla4 = pygame.image.load("Tiles1/frames2/tile221.png").convert()
-arbusto = pygame.image.load("Tiles1/frames2/tile69.png").convert()
-
-arbusto.set_colorkey((255,255,255)) 
+casilla4= pygame.image.load("Tiles2/casilla4.png").convert()
 
 #Cargando imagenes de avatares
 escudero=pygame.image.load('Tiles1/frames1/knight_m_idle_anim_f0.png')
 #pared_izquierda.set_colorkey(())
 
 #Cargando Mapas
-primer_escenario = mapa("mapa")
-primer_escenario = primer_escenario.split()
+mapa = mapa("mapa")
 
 #Creand fonts 
 font40 = pygame.font.SysFont('berlinsansfbdemi', 40)
@@ -85,7 +90,7 @@ def menu_principal():
         screen.fill((0,0,0))
 
         #Inciando Escenario de fondo
-        escenario()
+        escenario(-33)
 
         #Creando Texto
         TowerDefense = texto("Tower Defense",font40,(255,255,255),screen,200,50)
@@ -124,7 +129,7 @@ def opciones():
         screen.fill((0,0,0))
 
         #Inciando Escenario de fondo
-        escenario()
+        escenario(-33)
 
         #Creando Texto
         TowerDefense = texto("Tower Defense",font40,(255,255,255),screen,200,50)
@@ -151,7 +156,7 @@ def creditos():
         screen.fill((0,0,0))
 
         #Inciando Escenario de fondo
-        escenario()
+        escenario(-33)
 
         #Creando Texto
         TowerDefense = texto("Tower Defense",font40,(255,255,255),screen,200,50)
@@ -175,41 +180,50 @@ def creditos():
 #E: -
 #S: se crea el escenario en la pantalla
 #R: - 
-def escenario():
-    y = 0
-    for fila in primer_escenario:
+def escenario(y_actual):
+    global tiles
+    y = y_actual
+    y_display = 0 
+    for fila in mapa:
         x = 0
         for columna in fila:
-            display.blit(casilla3,(x*16,y*16))
+            if y_display <= 22:
+                display.blit(casilla4,(x*16,y*16))
+            elif y_display > 22 and y_display < 33:
+                display.blit(casilla2,(x*16,y*16))
+            else:
+                display.blit(casilla1,(x*16,y*16))
             x += 1
         y += 1
-    
-    y = 0
-    for fila in primer_escenario:
+        y_display += 1
+
+    y = y_actual
+    for fila in mapa:
         x = 0
         for columna in fila:
-            if columna == '1':
-                display.blit(arbusto,(x*16,y*16))
-            if columna == '2':
-                display.blit(arbusto,(x*16,y*16))
-            if columna == '3':
-                display.blit(arbusto,(x*16,y*16))
-            if columna == '4':
-                display.blit(arbusto,(x*16,y*16))
-            if columna == '5':
-                display.blit(arbusto,(x*16,y*16))
-            if columna == '6':
-                display.blit(arbusto,(x*16,y*16))
-            if columna == '7':
-                display.blit(arbusto,(x*16,y*16))
-            if columna == '8':
-                display.blit(arbusto,(x*16,y*16))
-
+            if columna == '0':
+                pass
+            else:
+                #print(columna)
+                imagen = buscar_tile(columna,tiles)
+                #print(imagen)
+                display.blit(imagen,(x*16,y*16))
             x += 1
         y += 1
 
-        screen.blit(pygame.transform.scale(display,(window_size)),(0,0))#Tansformando superficie a la escala de la ventana
-    
+    screen.blit(pygame.transform.scale(display,(window_size)),(0,0))#Tansformando superficie a la escala de la ventana
+
+#buscar_tiles(tile,tiles)
+#E: El nombre del tile que se busca y la lista de tiles
+#S: la superficie de esa tile
+#R: - 
+def buscar_tile(tile,tiles):
+    #print(tiles)
+    for y in tiles:
+        #print(y[0],tile)
+        if y[0] == tile:
+            #print(y[1])
+            return y[1]
 
 #juego()
 #E: -
@@ -219,6 +233,9 @@ def juego():
 
     #Iniciando ciclo
     running = True
+
+    #Inciando Scrolling
+    y = -33
     
     #Ciclo de juego
     while running:
@@ -231,7 +248,7 @@ def juego():
 
 
         #Reiniciando escenario
-        escenario()
+        escenario(y)
         
         #Ciclo de Eventos
         for event in pygame.event.get():
@@ -239,12 +256,17 @@ def juego():
                 running = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse = True
-                if mouse == True:
-                    running = False
+                if y < 0:
+                    meta = y + 11
+                    while y != meta:
+                        escenario(int(y))
+                        y += 0.25
+                        pygame.display.update()
+                    y = int(y)
+                else:
+                    pass
 
         pygame.display.update()
-
 
 menu_principal()
 
