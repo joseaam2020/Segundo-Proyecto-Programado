a986d007ddc669938a9150fc53e6aedf4d885ad1
Jkald3ron <alejandre030902@gmail.com>
2020-07-18 14:22:13 -0600

    Se agregaron carpetas para acomodar las imagenes a utilizar, se agregaron los sprites faltantes de los Avatares y se jicieron cambios en player.py y prueba de sprites.py

--- Prueba_de_sprites.py
+++ Prueba_de_sprites.py
@@ -1,16 +1,22 @@
 
 import pygame
 import player
+from random import *
 
 pygame.init()
-
 
 ancho_ventana = 640
 alto_ventana = 480
 screen = pygame.display.set_mode((ancho_ventana, alto_ventana))
 pygame.display.set_caption("Prueba de Sprites")
 clock = pygame.time.Clock()
-player = player.Escudero((ancho_ventana/2, alto_ventana))
+Escudero = player.Escudero((ancho_ventana/2, alto_ventana))
+canibal=player.Caníbal(((ancho_ventana/2), alto_ventana))
+arquero= player.Flechador(((ancho_ventana/2), alto_ventana))
+leñador= player.Leñador(((ancho_ventana/2), alto_ventana))
+Avatar=[Escudero,canibal,arquero,leñador]
+a=range(0,3)
+shuffle(Avatar)
 fin = False
 
 while fin == False:
@@ -19,11 +25,10 @@ while fin == False:
         if event.type == pygame.QUIT:
             fin = True
 
-    player.handle_event(event)
     screen.fill(pygame.Color('gray'))
-    screen.blit(player.image, player.rect)
-
+    Avatar[0].handle_event(event)
+    screen.blit(Avatar[0].image, Avatar[0].rect)
     pygame.display.flip()
-    clock.tick(2)
+    clock.tick(5)
 
 pygame.quit ()
--- player.py
+++ player.py
@@ -4,7 +4,7 @@ import pygame
 
 class Escudero(pygame.sprite.Sprite):
     def __init__(self, position):
-        self.sheet = pygame.image.load('sprites_escudero.png')
+        self.sheet = pygame.image.load('Avatares/Escudero/sprites_escudero.png')
         self.sheet.set_clip(pygame.Rect(0, 0, 16, 28))
         self.image = self.sheet.subsurface(self.sheet.get_clip())
         self.rect = self.image.get_rect()
@@ -42,4 +42,123 @@ class Escudero(pygame.sprite.Sprite):
     
         self.update('up')
 
+class Caníbal:
+    def __init__(self, position):
+        self.sheet = pygame.image.load('Avatares/Caníbal/caníbal_sprite.png')
+        self.sheet.set_clip(pygame.Rect(0, 0, 28, 28))
+        self.image = self.sheet.subsurface(self.sheet.get_clip())
+        self.rect = self.image.get_rect()
+        self.rect.topleft = position
+        self.frame = 0
+        self.up_states = { 0: (0, 0, 20, 28), 1: (22, 0, 16, 28), 2: (42, 0, 16, 28), 3:(61,0,16,28) }
 
+    def get_frame(self, frame_set):
+        self.frame += 1
+        if self.frame > (len(frame_set) - 1):
+            self.frame = 0
+        return frame_set[self.frame]
+
+    def clip(self, clipped_rect):
+        if type(clipped_rect) is dict:
+            self.sheet.set_clip(pygame.Rect(self.get_frame(clipped_rect)))
+        else:
+            self.sheet.set_clip(pygame.Rect(clipped_rect))
+        return clipped_rect
+
+    def update(self, direction):
+        if direction == 'up':
+            self.clip(self.up_states)
+            self.rect.y -= 5
+        if direction == 'stand_up':
+            self.clip(self.up_states[0])
+
+
+        self.image = self.sheet.subsurface(self.sheet.get_clip())
+
+    def handle_event(self, event):
+        if event.type == pygame.QUIT:
+            game_over = True
+
+    
+        self.update('up')
+
+class Flechador:
+    def __init__(self, position):
+        self.sheet = pygame.image.load('Avatares/Flechador/sprites_flechador.png')
+        self.sheet.set_clip(pygame.Rect(0, 0, 18, 28))
+        self.image = self.sheet.subsurface(self.sheet.get_clip())
+        self.rect = self.image.get_rect()
+        self.rect.topleft = position
+        self.frame = 0
+        self.up_states = { 0: (0, 0, 14, 28), 1: (19, 0, 14, 28), 2: (39, 0, 14, 28), 3:(58,0,18,28) }
+
+    def get_frame(self, frame_set):
+        self.frame += 1
+        if self.frame > (len(frame_set) - 1):
+            self.frame = 0
+        return frame_set[self.frame]
+
+    def clip(self, clipped_rect):
+        if type(clipped_rect) is dict:
+            self.sheet.set_clip(pygame.Rect(self.get_frame(clipped_rect)))
+        else:
+            self.sheet.set_clip(pygame.Rect(clipped_rect))
+        return clipped_rect
+
+    def update(self, direction):
+        if direction == 'up':
+            self.clip(self.up_states)
+            self.rect.y -= 5
+        if direction == 'stand_up':
+            self.clip(self.up_states[0])
+
+
+        self.image = self.sheet.subsurface(self.sheet.get_clip())
+
+    def handle_event(self, event):
+        if event.type == pygame.QUIT:
+            game_over = True
+
+    
+        self.update('up')
+
+class Leñador:
+    def __init__(self, position):
+        self.sheet = pygame.image.load('Avatares/Leñador/sprites_leñador.png')
+        self.sheet.set_clip(pygame.Rect(0, 0, 20, 28))
+        self.image = self.sheet.subsurface(self.sheet.get_clip())
+        self.rect = self.image.get_rect()
+        self.rect.topleft = position
+        self.frame = 0
+        self.up_states = { 0: (0, 0, 20, 28), 1: (23, 0, 20, 28), 2: (49, 0, 20, 28), 3:(73,0,20,28) }
+
+    def get_frame(self, frame_set):
+        self.frame += 1
+        if self.frame > (len(frame_set) - 1):
+            self.frame = 0
+        return frame_set[self.frame]
+
+    def clip(self, clipped_rect):
+        if type(clipped_rect) is dict:
+            self.sheet.set_clip(pygame.Rect(self.get_frame(clipped_rect)))
+        else:
+            self.sheet.set_clip(pygame.Rect(clipped_rect))
+        return clipped_rect
+
+    def update(self, direction):
+        if direction == 'up':
+            self.clip(self.up_states)
+            self.rect.y -= 5
+        if direction == 'stand_up':
+            self.clip(self.up_states[0])
+
+
+        self.image = self.sheet.subsurface(self.sheet.get_clip())
+
+    def handle_event(self, event):
+        if event.type == pygame.QUIT:
+            game_over = True
+
+    
+        self.update('up')
+        
